fe099b040cf912978a58d30ef291934bba29d7ef
Alexey Alexeew <lelikboss97@gmail.com>
2023-01-20 18:27:36 +0200

    complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,35 +20,51 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books, null, 4))
+public_users.get('/', async function (req, res) {
+   try {
+      res.send(JSON.stringify(books, null, 4))
+   } catch (error) {
+      res.status(500).send(error);
+   }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(books[isbn])
+public_users.get('/isbn/:isbn', async function (req, res) {
+    try {
+        const isbn = await  req.params.isbn;
+        res.send(books[isbn])
+    } catch (error) {
+        res.status(500).send(error);
+    }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author
-  let filtered_books = Object.values(books).filter(book => book.author === author)
-  if(filtered_books.length >= 1){
-      res.send(filtered_books)
-  } else {
-      res.send("Author not found.")
+public_users.get('/author/:author',async function (req, res) {
+  try {
+        const author = await req.params.author
+        let filtered_books =  Object.values(books).filter(book => book.author === author)
+        if(filtered_books.length >= 1){
+            res.send(filtered_books)
+        } else {
+            res.send("Author not found.")
+        }
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title
-    let filtered_books = Object.values(books).filter(book => book.title === title)
-    if(filtered_books.length >= 1){
-        res.send(filtered_books)
-    } else {
-        res.send("Book not found.")
+public_users.get('/title/:title',async function (req, res) {
+    try {
+        const title = await req.params.title
+        let filtered_books = Object.values(books).filter(book => book.title === title)
+        if(filtered_books.length >= 1){
+            res.send(filtered_books)
+        } else {
+            res.send("Book not found.")
+        }
+    } catch (error) {
+        
     }
 });
 
